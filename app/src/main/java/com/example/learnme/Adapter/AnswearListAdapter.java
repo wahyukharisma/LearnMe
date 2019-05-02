a182b0bd76b2de52bf74379bf239d88d7c3b2a0f
Wahyu Kharisma <madewahyu39G@gmail.com>
2019-05-03 03:39:03 +0700

    Adding activity for handle answear question

--- app/src/main/java/com/example/learnme/Adapter/AnswearListAdapter.java
+++ app/src/main/java/com/example/learnme/Adapter/AnswearListAdapter.java
@@ -1,37 +1,65 @@
 package com.example.learnme.Adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.learnme.Model.Answear;
+import com.example.learnme.R;
 
 import java.util.List;
 
 public class AnswearListAdapter extends RecyclerView.Adapter<AnswearListAdapter.AnswearListViewHolder> {
 
+    Context mContext;
     List<Answear> mData;
+
+    public AnswearListAdapter(Context mContext, List<Answear> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
 
     @NonNull
     @Override
     public AnswearListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view                     = mInflater.inflate(R.layout.list_item_answear,viewGroup,false);
+        return new AnswearListViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AnswearListViewHolder answearListViewHolder, int i) {
 
+        answearListViewHolder.txt_desc.setText(mData.get(i).getAnswear());
+        answearListViewHolder.txt_name.setText(mData.get(i).getName());
+        answearListViewHolder.txt_dislike.setText(Integer.toString(mData.get(i).getDislike()));
+        answearListViewHolder.txt_like.setText(Integer.toString(mData.get(i).getLike()));
+        answearListViewHolder.txt_date.setText(mData.get(i).getDate());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
     }
 
     public class AnswearListViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txt_name,txt_desc,txt_date,txt_like,txt_dislike;
+
         public AnswearListViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            txt_name    = (TextView) itemView.findViewById(R.id.txt_user_comment);
+            txt_desc    = (TextView) itemView.findViewById(R.id.txt_desc_comment);
+            txt_date    = (TextView) itemView.findViewById(R.id.txt_date_comment);
+            txt_like    = (TextView) itemView.findViewById(R.id.txt_like);
+            txt_dislike = (TextView) itemView.findViewById(R.id.txt_dislike);
         }
     }
 }
--- app/src/main/java/com/example/learnme/QuestionExpand.java
+++ app/src/main/java/com/example/learnme/QuestionExpand.java
@@ -3,8 +3,19 @@ package com.example.learnme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.learnme.Adapter.AnswearListAdapter;
+import com.example.learnme.Adapter.QuestionListAdapter;
+import com.example.learnme.Model.Answear;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuestionExpand extends AppCompatActivity {
+
+    List<Answear> mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +23,22 @@ public class QuestionExpand extends AppCompatActivity {
         setContentView(R.layout.activity_question_expand);
 
         Intent intent = getIntent();
+
+        // initialize recycle view item trending
+        mData = new ArrayList<>();
+        addData(mData);
+
+        // recycle view answear listener
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycle_view_answear);
+        AnswearListAdapter myAdapter   = new AnswearListAdapter(this.getApplicationContext(),mData);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(myAdapter);
+    }
+
+    private void addData(List<Answear> mData){
+        for(int i=0;i<5;i++){
+            mData.add(new Answear("Wahyu Kharisma","Lorem Ipsum Dolor Sit Amet, Solasido Doremi Fasolasido Lumpur Lapindo, Proyek Batubara, Lorem Ipsum Dolor Sit Amet, Solasido Doremi Fasolasido Lumpur Lapindo, Proyek Batubara","1 May 2019",2,1));
+        }
     }
 }
